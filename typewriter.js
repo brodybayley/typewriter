e8c84658dd174aae4ce6c1246f4e0b6ca7ebdcdc
Brody Bayley <brodybayley@gmail.com>
2021-02-09 22:39:05 +0000

    pushing through eslint fixes

--- typewriter.js
+++ typewriter.js
@@ -4,18 +4,18 @@ const finalString = () => {
   let count = 0;
   for (const char of sentence) {
     setTimeout(() => {
-      process.stdout.write(char)
-    }, count * 50)
+      process.stdout.write(char);
+    }, count * 50);
     count++;
-  };
+  }
 
   setTimeout(() => {
     process.stdout.write('\n');
-  }, sentence.length * 50)
-}
+  }, sentence.length * 50);
+};
 
 
-finalString()
+finalString();
 
 
 
