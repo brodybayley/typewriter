c08365d949492fe0ab4fc152da1956e3b97a222a
Brody Bayley <brodybayley@gmail.com>
2021-02-09 22:01:59 +0000

    pushing through updates to function

--- typewriter.js
+++ typewriter.js
@@ -1,17 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
-// for (const char of sentence) {
-//   setTimeout(() => {
-//     process.stdout.write(char);
-//   }, 50)
-// }
-
 for (const char of sentence) {
   delay(char)
-}
+};
 
 function delay(char) {
   setTimeout(() => {
     process.stdout.write(char)
-  }, 10000)
+  }, 50)
 }
+
+console.log(delay())
+
